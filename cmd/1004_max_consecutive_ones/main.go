f77b05e0ec882d9a3238d9b16129e0d3d14b8316
FadyGamilM <gamilfady605@gmail.com>
2024-11-08 00:50:24 +0200

    sliding window | problem 1

---
+++ cmd/1004_max_consecutive_ones/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"math"
+)
+
+func main() {
+	log.Println(longestOnes([]int{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}, 2))
+	log.Println(longestOnes([]int{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3))
+	log.Println(longestOnes([]int{0, 0, 0, 1}, 4))
+}
+
+func longestOnes(nums []int, k int) int {
+	flippedZeros := 0
+	start := 0
+	maxSoFar := math.MinInt
+	end := 0
+	for i, num := range nums {
+		end = i
+		if num == 0 {
+			if flippedZeros >= k {
+				maxSoFar = int(math.Max(float64(maxSoFar), float64(float64(end)-float64(start))))
+				for flippedZeros >= k {
+					if nums[start] == 0 {
+						flippedZeros--
+						start++
+					} else {
+						start++
+					}
+				}
+				flippedZeros++
+			} else {
+				flippedZeros++
+			}
+		}
+	}
+	maxSoFar = int(math.Max(float64(maxSoFar), float64(float64(end)-float64(start)+1)))
+
+	return maxSoFar
+}
